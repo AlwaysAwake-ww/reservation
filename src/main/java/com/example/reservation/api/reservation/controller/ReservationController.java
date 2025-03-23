93b12d3dc022959b387ad0402d6cfcd634edc689
ww <robinia234@naver.com>
2025-03-23 19:30:29 +0900

    2025-03-23

--- src/main/java/com/example/reservation/api/reservation/controller/ReservationController.java
+++ src/main/java/com/example/reservation/api/reservation/controller/ReservationController.java
@@ -6,7 +6,11 @@ import com.example.reservation.api.reservation.service.ReservationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
@@ -23,4 +27,11 @@ public class ReservationController {
         return null;
     }
 
+
+    @PostMapping("/savaList")
+    public ReservationResponseDto addReservation(@RequestBody Map<String, Object> param){
+
+
+        return null;
+    }
 }
